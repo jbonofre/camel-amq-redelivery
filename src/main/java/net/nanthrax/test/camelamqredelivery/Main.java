0e376bfebaef45c36069f865227ecb947bf8d64a
Jean-Baptiste Onofré <jbonofre@apache.org>
2019-05-07 17:47:17 +0200

    Show ActiveMQ redelivery usage with long delay

--- src/main/java/net/nanthrax/test/camelamqredelivery/Main.java
+++ src/main/java/net/nanthrax/test/camelamqredelivery/Main.java
@@ -1,8 +1,6 @@
 package net.nanthrax.test.camelamqredelivery;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.impl.SimpleRegistry;
@@ -11,22 +9,19 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         DefaultCamelContext camelContext = new DefaultCamelContext();
-        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616?jms.redeliveryPolicy.maximumRedeliveries=5&jms.redeliveryPolicy.redeliveryDelay=84000");
         SimpleRegistry registry = new SimpleRegistry();
         registry.put("connectionFactory", connectionFactory);
         camelContext.setRegistry(registry);
         camelContext.addRoutes(new RouteBuilder() {
             @Override
             public void configure() throws Exception {
-                from("jms:queue:test?connectionFactory=#connectionFactory&acknowledgementModeName=CLIENT_ACKNOWLEDGE")
+                errorHandler(noErrorHandler());
+                from("jms:queue:test?connectionFactory=#connectionFactory&cacheLevelName=CACHE_CONSUMER&acknowledgementModeName=CLIENT_ACKNOWLEDGE")
                         .log("Message received")
                         .choice()
                         .when(simple("${body} contains 'good'")).log("Good message: ${body}")
-                        .otherwise().process(new Processor() {
-                    public void process(Exchange exchange) throws Exception {
-                        throw new IllegalArgumentException("Bad message");
-                    }
-                });
+                        .otherwise().throwException(new IllegalArgumentException("Bad message"));
             }
         });
         camelContext.start();
